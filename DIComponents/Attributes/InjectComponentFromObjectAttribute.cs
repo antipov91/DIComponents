27f0547f62d0b646edb816882afe3704c86bd49b
valant777 <25293147+valant777@users.noreply.github.com>
2019-08-13 19:25:11 +0300

    fix namespace

---
+++ DIComponents/Attributes/InjectComponentFromObjectAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIComponents
+{
+	public class InjectComponentFromObjectAttribute : Attribute
+	{
+		public string ObjectName { get; private set; }
+
+		public InjectComponentFromObjectAttribute(string objectName)
+		{
+			ObjectName = objectName;
+		}
+	}
+}
